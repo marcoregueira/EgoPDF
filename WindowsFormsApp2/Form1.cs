a9cad21c262abab77c2308b590325aada2a543b8
Marco Regueira <marcoregueira@gmail.com>
2017-08-02 18:30:44 +0200

    Aplicación 2 funciona

--- WindowsFormsApp2/Form1.cs
+++ WindowsFormsApp2/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Ego.PDF;
 
 namespace WindowsFormsApp2
 {
@@ -15,6 +16,11 @@ namespace WindowsFormsApp2
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var t = new FPdf();
         }
     }
 }
