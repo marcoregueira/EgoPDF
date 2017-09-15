f7bd981f923b6d5aabe61065af89f7ffb64d348c
Marco Regueira <marcoregueira@gmail.com>
2017-09-15 16:26:08 +0200

    Version bump

--- Ego.PdfCore/FPdf.cs
+++ Ego.PdfCore/FPdf.cs
@@ -5,8 +5,6 @@ using System.Globalization;
 using System.IO;
 using System.IO.Compression;
 using System.Linq;
-using System.Reflection;
-using System.Security.Cryptography;
 using System.Text;
 
 using MiscUtil.Conversion;
@@ -19,7 +17,6 @@ using Ego.PDF.Support;
 
 using Microsoft.Xna.Framework;
 using static Ego.PDF.Printf.SprintfTools;
-using Ego.PdfCore.Support;
 
 /*******************************************************************************
 * FPDF                                                                         *
