341e4ffbbd3a0f1561254fed672fbb24f7fe6afd
Marco Regueira <marcoregueira@gmail.com>
2020-07-21 03:11:49 +0200

    Fix packages 3

--- Support/FontImport/FontGenerators.cs
+++ Support/FontImport/FontGenerators.cs
@@ -1,12 +1,10 @@
 ﻿using Ego.PDF;
 using Ego.PDF.Data;
 using Ego.PDF.Font;
-using Newtonsoft.Json;
 using System;
 using System.IO;
 using System.Linq;
 using System.Reflection;
-using System.Windows.Media;
 using Xunit;
 
 namespace FontImport
@@ -14,6 +12,13 @@ namespace FontImport
     //TODO: MIGRATE TO .NET CORE 3.0
     public class FontGenerators
     {
+
+        private string GetPath()
+        {
+            var codeBase = this.GetType().GetTypeInfo().Assembly.CodeBase.Replace("file:///", "");
+            return Path.GetDirectoryName(codeBase);
+        }
+
         [Fact]
         public void GenerateArial()
         {
@@ -29,32 +34,27 @@ namespace FontImport
             }
         }
 
-        private string GetPath()
-        {
-            var codeBase = Assembly.GetExecutingAssembly().CodeBase.Replace("file:///","");
-            return Path.GetDirectoryName(codeBase);
-        }
-
         public FontDefinition LoadFont(string name, string path)
         {
-            var font = new GlyphTypeface(new Uri("file:///" + path));
-            var fontData = new FontDefinition
-            {
-                FontType = FontTypeEnum.TrueType,
-                FontFile = path,
-                Name = font.FamilyNames.FirstOrDefault().Value.ToLower(),
-            };
-
-            foreach (var advance in font.AdvanceWidths)
-            {
-                var leftBearing = font.LeftSideBearings[advance.Key];
-                var rightBearing = font.RightSideBearings[advance.Key];
-                fontData.Widths[Convert.ToChar(advance.Key).ToString()] = advance.Value * 1000;
-            }
-
-            FontBuilder.Fonts[name.ToLower()] = fontData;
-
-            return fontData;
+            return null;
+            //    var font = new GlyphTypeface(new Uri("file:///" + path));
+            //    var fontData = new FontDefinition
+            //    {
+            //        FontType = FontTypeEnum.TrueType,
+            //        FontFile = path,
+            //        Name = font.FamilyNames.FirstOrDefault().Value.ToLower(),
+            //    };
+            //
+            //    foreach (var advance in font.AdvanceWidths)
+            //    {
+            //        var leftBearing = font.LeftSideBearings[advance.Key];
+            //        var rightBearing = font.RightSideBearings[advance.Key];
+            //        fontData.Widths[Convert.ToChar(advance.Key).ToString()] = advance.Value * 1000;
+            //    }
+            //
+            //    FontBuilder.Fonts[name.ToLower()] = fontData;
+            //
+            //    return fontData;
         }
     }
 }
