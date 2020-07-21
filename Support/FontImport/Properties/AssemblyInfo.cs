7783311d071b20cc4eba2ab1a2894631976aa204
Marco Regueira <marcoregueira@gmail.com>
2020-07-21 03:15:38 +0200

    Fix packages 3

--- Support/FontImport/Properties/AssemblyInfo.cs
+++ Support/FontImport/Properties/AssemblyInfo.cs
@@ -2,17 +2,7 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("FontImport")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("FontImport")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
