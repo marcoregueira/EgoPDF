19f6ab674b7105c416090997fee18a7adcbab580
Marco Regueira <marcoregueira@gmail.com>
2024-04-02 15:58:50 +0200

    Replace demo site

--- WebDemo/Controllers/SampleDataController.cs
+++ WebDemo/Controllers/SampleDataController.cs
@@ -1,79 +1,81 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
 using Ego.PDF.Samples;
 using Microsoft.AspNetCore.Mvc;
 
 namespace WebDemo.Controllers
 {
-    [Route("api/[controller]")]
+    [ApiController]
+    [Route("api/[controller]/[action]")]
     public class SampleDataController : Controller
     {
+
+        public SampleDataController()
+        {
+        }
+
         private string CurrentPath
         {
             get
             {
-                return AppDomain.CurrentDomain.GetData("DataDirectory").ToString();
+                var path = Path.Combine(Environment.CurrentDirectory, "App_Data");
+                return path;
             }
         }
 
-        [HttpGet("[action]")]
+        [HttpGet()]
         public FileStreamResult GetSample1()
         {
-            var buffer =  Sample1.GetSample(null);
+            var buffer = Sample1.GetSample(null);
             buffer.Seek(0, SeekOrigin.Begin);
             var result = new FileStreamResult(buffer, "application/pdf");
             return result;
         }
 
-        [HttpGet("[action]")]
+        [HttpGet()]
         public FileStreamResult GetSample2()
         {
-            var buffer =  Sample2.GetSample(null, Path.Combine(CurrentPath, "logo.png"));
+            var buffer = Sample2.GetSample(null, Path.Combine(CurrentPath, "logo.png"));
             buffer.Seek(0, SeekOrigin.Begin);
             var result = new FileStreamResult(buffer, "application/pdf");
             return result;
         }
 
-        [HttpGet("[action]")]
+        [HttpGet()]
         public FileStreamResult GetSample2b()
         {
-            var buffer =  Sample2.GetSample(null, Path.Combine(CurrentPath, "3d_down.png"));
+            var buffer = Sample2.GetSample(null, Path.Combine(CurrentPath, "3d_down.png"));
             buffer.Seek(0, SeekOrigin.Begin);
             var result = new FileStreamResult(buffer, "application/pdf");
             return result;
         }
 
-        [HttpGet("[action]")]
+        [HttpGet()]
         public FileStreamResult GetSample2c()
         {
-            var buffer =  Sample2.GetSample(null, Path.Combine(CurrentPath, "gift.jpg"));
+            var buffer = Sample2.GetSample(null, Path.Combine(CurrentPath, "gift.jpg"));
             buffer.Seek(0, SeekOrigin.Begin);
             var result = new FileStreamResult(buffer, "application/pdf");
             return result;
         }
 
-        [HttpGet("[action]")]
+        [HttpGet()]
         public FileStreamResult GetSample3()
         {
-            var buffer =  Sample3.GetSample(null, CurrentPath);
+            var buffer = Sample3.GetSample(null, CurrentPath);
             buffer.Seek(0, SeekOrigin.Begin);
             var result = new FileStreamResult(buffer, "application/pdf");
             return result;
         }
 
-        [HttpGet("[action]")]
+        [HttpGet()]
         public FileStreamResult GetSample4()
         {
-            var buffer =  Sample4.GetSample(null, CurrentPath);
+            var buffer = Sample4.GetSample(null, CurrentPath);
             buffer.Seek(0, SeekOrigin.Begin);
             var result = new FileStreamResult(buffer, "application/pdf");
             return result;
         }
 
-        [HttpGet("[action]")]
+        [HttpGet()]
         public FileStreamResult GetSample5()
         {
             var buffer = Sample5.GetSample(null, CurrentPath);
@@ -82,19 +84,19 @@ namespace WebDemo.Controllers
             return result;
         }
 
-        [HttpGet("[action]")]
+        [HttpGet()]
         public FileStreamResult GetSample6()
         {
-            var buffer =  Sample6.GetSample(null, CurrentPath);
+            var buffer = Sample6.GetSample(null, CurrentPath);
             buffer.Seek(0, SeekOrigin.Begin);
             var result = new FileStreamResult(buffer, "application/pdf");
             return result;
         }
 
-        [HttpGet("[action]")]
+        [HttpGet()]
         public FileStreamResult GetSample8()
         {
-            var buffer =  Sample8.GetSample(null, CurrentPath);
+            var buffer = Sample8.GetSample(null, CurrentPath);
             buffer.Seek(0, SeekOrigin.Begin);
             var result = new FileStreamResult(buffer, "application/pdf");
             return result;
--- WebDemo/Program.cs
+++ WebDemo/Program.cs
@@ -1,24 +1,43 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
-
-namespace WebDemo
+namespace WebDemoCore
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
-        }
+            var builder = WebApplication.CreateBuilder(args);
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+            // Add services to the container.
+            builder.Services.AddRazorPages();
+            builder.Services.AddControllers();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json",
+                "Reports API");
+            });
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapRazorPages();
+            app.MapControllers();
+            app.Run();
+        }
     }
 }
