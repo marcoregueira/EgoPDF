9a90643a70b784b3dfa56fe053ecd4b256637c11
Marco Regueira <marcoregueira@gmail.com>
2024-04-03 14:19:24 +0200

    Tests with multiple fonts. Png support update

---
+++ Ego.Pdf.Test/MyType/readme.txt
@@ -0,0 +1,9 @@
+My Type of Font - Nov 20th 2000
+
+1001 Free Fonts - Jason Nolan
+
+This font is 100% freeware and may be distributed as long as this readme file is attached.
+
+JasonNolan@aol.com
+
+PS : Special thanks to Dan at http://www.iconian.com for his much needed help.
---
+++ Ego.PdfCore/PngConstants.cs
@@ -0,0 +1,33 @@
+﻿/*******************************************************************************
+* FPDF                                                                         *
+*                                                                              *
+* Version: 1.7                                                                 *
+* Date:    2011-06-18                                                          *
+* Author:  Olivier PLATHEY                                                     *
+*******************************************************************************/
+
+/*******************************************************************************
+* FPDF.net                                                                     *
+* .NET port and adaptation                                                     *
+* Version: 1.0 prealpha                                                        *
+* Date:    2012-09-30                                                          *
+* Author:  Marco Antonio Regueira                                              *
+*******************************************************************************/
+
+namespace Ego.PDF
+{
+    public static class PngConstants
+    {
+        // PNG SUBTYPES
+        public const string IHDR = "IHDR";
+        public const string PLTE = "PLTE";
+        public const string IDAT = "IDAT";
+        public const string IEND = "IEND";
+        public const string tRNS = "tRNS";
+        public const string pHYs = "pHYs";
+        public const string gAMA = "gAMA";
+        public const string cHRM = "cHRM";
+        public const string sRGB = "sRGB";
+        public const string iCCP = "iCCP";
+    }
+}
